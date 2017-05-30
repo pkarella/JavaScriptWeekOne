4255d75600f61564842842d9e737162ba41a0167
Elan Baharie <0elan1@gmail.com>
2017-05-30 16:15:44 -0700

    final

--- js/doctor.js
+++ js/doctor.js
@@ -7,15 +7,15 @@ Doctor.prototype.doctors = function(illness) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ illness+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=' + apiKey)
    .then(function(response) {
      console.log(response)
-     console.log(typeof response.data)
-     response.data.forEach(function(physician){
-       $('#result').append("<li><h3> Name:</h3> " + physician.profile.last_name + ", " + physician.profile.first_name + " " + physician.profile.title+"</li>");
-       $('#result').append( "<li><h3>Biography:</h3> " + physician.profile.bio + "</li>");
-       $('#result').append('<img src="'+ physician.profile.image_url + '"</img>');
-      });
-     })
+     console.log( response.data.length)
+     for (i=0;i<response.data.length; i++){
+       $('#result').append("<li><h3> Name:</h3> " + response.data[i].profile.last_name + ", " + response.data[i].profile.first_name + " " + response.data[i].profile.title+"</li>");
+       $('#result').append( "<li><h3>Biography:</h3> " + response.data[i].profile.bio + "</li>");
+       $('#result').append('<img src="'+ response.data[i].profile.image_url + '"</img>');
+     }
+   })
      .fail(function(error){
      });
-     };
+    };
 
      exports.doctorModule = Doctor;
